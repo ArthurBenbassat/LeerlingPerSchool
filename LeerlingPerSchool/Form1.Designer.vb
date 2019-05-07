b49e8e279ed8abd0aa1f9d9186aff0b72674905b
Arthur Benbassat <arthur.benbassat@outlook.com>
2019-05-07 14:02:14 +0200

    sqdf

--- LeerlingPerSchool/Form1.Designer.vb
+++ LeerlingPerSchool/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,35 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(65, 36)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(581, 342)
+        Me.ListBox1.TabIndex = 0
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
-    End Sub
+        Me.ResumeLayout(False)
 
+    End Sub
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
 End Class
--- LeerlingPerSchool/Form1.vb
+++ LeerlingPerSchool/Form1.vb
@@ -1,3 +1,50 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        With OpenFileDialog1
+            .InitialDirectory = "C:\Users\Arthur\Downloads\"
+        End With
+        Dim lijn, school, klas, naam, filterklas, filterschool, totaalllnschool As String
+        Dim einde As Boolean
+        Dim totaalllnklas As Integer
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            FileOpen(1, OpenFileDialog1.FileName, OpenMode.Input)
+            Try
+                lijn = LineInput(1)
+                school = lijn.Substring(0, 29).Trim
+                klas = lijn.Substring(29, 5).Trim
+                naam = lijn.Substring(33).Trim
+            Catch ex As Exception
+                einde = True
+            End Try
+            Do While Not einde
+                filterschool = school
+                totaalllnschool = 0
+                ListBox1.Items.Add("Het aantal leerlingen van " & filterschool & " is:")
+                Do While Not einde And school = filterschool
+                    totaalllnschool += totaalllnklas
+                    filterklas = klas
+                    totaalllnklas = 0
+                    Do While Not einde And klas = filterklas
+                        totaalllnklas += 1
+                        Try
+                            lijn = LineInput(1)
+                            school = lijn.Substring(0, 29).Trim
+                            klas = lijn.Substring(29, 5).Trim
+                            naam = lijn.Substring(33).Trim
+                        Catch ex As Exception
+                            einde = True
+                        End Try
+
+                    Loop
+                    ListBox1.Items.Add("Het aantal leerlingen van " & filterklas & " is " & totaalllnklas)
+                Loop
+                ListBox1.Items.Add(vbCrLf)
+                ListBox1.Items.Add("Het totaal is: " & totaalllnschool)
+                ListBox1.Items.Add(vbCrLf)
+            Loop
+
+        End If
+
+    End Sub
 End Class
